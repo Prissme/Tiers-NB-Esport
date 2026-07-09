462293124c85f081e362b76d822e687b638589b5
Prissme <drissbouhaiat@gmail.com>
2026-07-09 09:52:27 +0200

    Update brawler-priority.ts

--- app/lib/brawler-priority.ts
+++ app/lib/brawler-priority.ts
@@ -9,7 +9,7 @@ export const MAP_PRIORITY: Record<string, 0 | 1 | 2> = {
   Gus: 1, Ruffs: 1, Carl: 1, Otis: 1, Alli: 1, Griff: 1, Meeple: 1, Squeak: 1, Surge: 1,
   Sam: 1, Maisie: 1, Penny: 1, Gale: 1, Janet: 1, Amber: 1, Lily: 1, Rico: 1, Pearl: 1,
   Edgar: 1, Nita: 1, Leon: 1, Kenji: 1, Melodie: 1, Byron: 1, Poco: 1, Draco: 1, Glowy: 1,
-  Colette: 1, Ziggy: 1, Bibi: 1, Lou: 1, Fang: 1, Ash: 1, Nova: 1, Meg: 1, Shade: 1,
+  Colette: 1, Ziggy: 1, Bibi: 1, Lou: 1, Fang: 1, Ash: 1, Nova: 1, Meg: 1, Shade: 1, Mico: 1,
   Clancy: 1, Max: 1, Jessie: 1, "El Primo": 1, Darryl: 1,
 
   Trunk: 0, Hank: 0, Frank: 0,
@@ -114,7 +114,7 @@ export function getCounterEffect(brawler: string, opponentComp: string[]): numbe
 export const SUPPORTS = new Set(["Gus", "Pam", "Ruffs", "Poco", "Byron", "Lumi", "Kit", "Gray", "Berry"]);
 export const MELEES = new Set([
   "Frank", "Bull", "Hank", "Ash", "El Primo", "Mortis", "Sam", "Kenji", "Lily", "Rosa",
-  "Darryl", "Draco", "Trunk", "Shade", "Damian", "Bolt", "Bibi", "Edgar", "Fang", "Nova", "Meg",
+  "Darryl", "Draco", "Trunk", "Shade", "Damian", "Bolt", "Bibi", "Edgar", "Fang", "Nova", "Meg", "Mico",
 ]);
 export const SNIPERS_POKE = new Set([
   "Piper", "Belle", "Brock", "Colt", "Rico", "Maisie", "Janet", "Najia", "Colette", "Dynamike",
