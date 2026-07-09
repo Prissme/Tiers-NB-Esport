4549d5251025b7041670986a3918cfd2e3e3069e
Prissme <drissbouhaiat@gmail.com>
2026-07-09 10:22:22 +0200

    Add Barley and Bea to priority lists

--- app/lib/brawler-priority.ts
+++ app/lib/brawler-priority.ts
@@ -7,7 +7,7 @@ export const MAP_PRIORITY: Record<string, 0 | 1 | 2> = {
 
   Bo: 1, Berry: 1, Stu: 1, Brock: 1, Sandy: 1, Rosa: 1, Mortis: 1, Buster: 1, Pam: 1, Chuck: 1,
   Gus: 1, Ruffs: 1, Carl: 1, Otis: 1, Alli: 1, Griff: 1, Meeple: 1, Squeak: 1, Surge: 1,
-  Sam: 1, Maisie: 1, Penny: 1, Gale: 1, Janet: 1, Amber: 1, Lily: 1, Rico: 1, Pearl: 1,
+  Sam: 1, Maisie: 1, Penny: 1, Gale: 1, Janet: 1, Amber: 1, Lily: 1, Rico: 1, Pearl: 1, Barley: 1, Bea: 1,
   Edgar: 1, Nita: 1, Leon: 1, Kenji: 1, Melodie: 1, Byron: 1, Poco: 1, Draco: 1, Glowy: 1,
   Colette: 1, Ziggy: 1, Bibi: 1, Lou: 1, Fang: 1, Ash: 1, Nova: 1, Meg: 1, Shade: 1, Mico: 1,
   Clancy: 1, Max: 1, Jessie: 1, "El Primo": 1, Darryl: 1,
@@ -118,7 +118,7 @@ export const MELEES = new Set([
 ]);
 export const SNIPERS_POKE = new Set([
   "Piper", "Belle", "Brock", "Colt", "Rico", "Maisie", "Janet", "Najia", "Colette", "Dynamike",
-  "Bo", "Griff", "Pierce", "Angelo", "Pearl", "Charlie", "Penny", "Lou", "Amber",
+  "Bo", "Griff", "Pierce", "Angelo", "Pearl", "Charlie", "Penny", "Lou", "Amber", "Barley", "Bea",
 ]);
 export const DIVE_UNITS = new Set([
   "Mortis", "Alli", "Crow", "Lily", "Kenji", "Melodie", "Glowy", "Sirius", "Ziggy", "Stu", "Bolt", "Edgar", "Leon", "Fang", "Shade",
